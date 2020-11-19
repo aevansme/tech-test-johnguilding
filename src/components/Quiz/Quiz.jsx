80589497161b7004fc4ee2b8d8aa504d16a9202a
John Guilding <jwguilding@gmail.com>
2020-11-19 15:04:21 +0000

    quiz functionality done

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Quiz.module.scss";
 import { useForm } from "react-hook-form";
 
 const Quiz = () => {
 
-  const { submit, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
+  const [ input, setInput ] = useState(null);
+  const [ answer, setAnswer ] = useState(null)
 
   const onSubmit = (data) => {
-    console.log(data);
+    const userInput = data.answer;
+    console.log(userInput);
+    setInput(userInput)
+    console.log(userInput);
   }
+
+  useEffect(() => {
+    if(input == 4) {
+      setAnswer(<h2>Correct! The answer is 4</h2>)
+    } else if (input === null) {
+      setAnswer(<h2></h2>)
+    } else {
+      setAnswer(<h2>Incorrect!</h2>)
+    }
+  }, [input])
+
 
   return (
     <>
       <div className={styles.quiz}>
         <h2>What is 2 X 2?</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="question"></label>
-          <input type="text" ref={submit} name="question" placeholder="enter your answer here" />
-        <button type="submit">Submit</button>
+          <label htmlFor="answer"></label>
+          <input type="text" ref={register} id="answer" name="answer" placeholder="enter your answer here" />
+        <input type="submit" className={styles.submit}/>
         </form>
-        <div className={styles.answer}></div>
+        <div className={styles.answer}>
+          {answer}
+        </div>
       </div>
     </>
   );
